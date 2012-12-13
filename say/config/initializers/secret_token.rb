de8a881070fbda68261c2e482a94338363bb0851
Jayanth Venkat <jvenkata@ubuntu.(none)>
2012-12-12 16:21:53 -0800

    Adding rails files

---
+++ say/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Say::Application.config.secret_token = '36bc99ad9937b0cd1137e4c7fad74dec4e226df3281382427a7c4afe378df2938a65a2a613f426c99348af21f25b52325ee6dbc6e706976f44f703ee3687fd62'
