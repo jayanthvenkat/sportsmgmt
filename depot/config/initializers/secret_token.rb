18e14f41819579a42491539e1c5729bd5a872f59
Jayanth Venkat <jvenkata@ubuntu.(none)>
2012-12-12 17:48:35 -0800

    Adding the Depot application

---
+++ depot/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '6d1426fe0d45b1f2f5bf892d090f1b4556a4e67ef26ebc309ab843abb6c766779aa2cf1ad2dd509704c5be24db464814cfac3e8a32e20b5acf69a3dc88a6974f'
